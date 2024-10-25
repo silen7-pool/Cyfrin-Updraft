a0df7416a4be8e27403c49518357153c0219aacc
silent-pooler <mo7trading@gmail.com>
2024-10-25 15:54:19 +0100

    last commit on foundry simple storage

--- foundry-simple-storage/src/SimpleStorage.sol
+++ foundry-simple-storage/src/SimpleStorage.sol
@@ -12,7 +12,7 @@ contract SimpleStorage {
         uint256 favoriteNumber;
         string name;
     }
-    // uint256[] public anArray;
+
     Person[] public listOfPeople;
 
     mapping(string => uint256) public nameToFavoriteNumber;
