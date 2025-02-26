789e68b25af66234a6b7ace8b4604998884ccf80
silen7-pool <mo7trading@gmail.com>
2025-02-26 15:45:48 +0000

    deploy script + first test of raffle contract success

--- foundry-lottery/script/DeployRaffle.s.sol
+++ foundry-lottery/script/DeployRaffle.s.sol
@@ -1,20 +1,49 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
-
-import {Script, console} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 import {Raffle} from "../src/Raffle.sol";
 
+//import {AddConsumer, CreateSubscription, FundSubscription} from "./Interactions.s.sol";
+
 contract DeployRaffle is Script {
-    Raffle public raffle;
+    function run() external returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
+        // AddConsumer addConsumer = new AddConsumer();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
-    function setUp() public {}
+        // if (config.subscriptionId == 0) {
+        //     CreateSubscription createSubscription = new CreateSubscription();
+        //     (config.subscriptionId, config.vrfCoordinatorV2_5) =
+        //         createSubscription.createSubscription(config.vrfCoordinatorV2_5, config.account);
 
-    function run() public {
-        vm.startBroadcast();
+        //     FundSubscription fundSubscription = new FundSubscription();
+        //     fundSubscription.fundSubscription(
+        //         config.vrfCoordinatorV2_5, config.subscriptionId, config.link, config.account
+        //     );
 
-        //raffle = new Raffle();
+        //     helperConfig.setConfig(block.chainid, config);
+        // }
 
+        vm.startBroadcast(config.account);
+        Raffle raffle = new Raffle(
+            config.subscriptionId,
+            config.gasLane,
+            config.automationUpdateInterval,
+            config.raffleEntranceFee,
+            config.callbackGasLimit,
+            config.vrfCoordinatorV2_5
+        );
         vm.stopBroadcast();
+
+        // We already have a broadcast in here
+        // addConsumer.addConsumer(
+        //     address(raffle),
+        //     config.vrfCoordinatorV2_5,
+        //     config.subscriptionId,
+        //     config.account
+        // );
+        return (raffle, helperConfig);
     }
 }
